ae3723c68bc581d405432257597ee9312f70e8e9
Bradley Boxer <brad.boxer1@gmail.com>
2016-10-10 20:14:33 -0400

    Autonomous2 Variable Names
    Made variable names in Autonomous2 actually make sense. Changed some
    comments too.

--- src/org/usfirst/frc/team303/robot/Autonomous2.java
+++ src/org/usfirst/frc/team303/robot/Autonomous2.java
@@ -5,10 +5,9 @@ import edu.wpi.first.wpilibj.Timer;
 public class Autonomous2 {
 
 	/*
-	sLow means the low-bounds of the second it returns true
-	sHigh means the high-bounds of the second it returns true, if using encoders this is usually 'sEnd'
-	eLow means the low-bounds of the encoder tick it returns true
-	eHigh means the high-bounds of the encoder tick it returns true, if using to make sure the previous action was completed it will be 'eEnd'. 
+	e means encoders
+	s means seconds
+	n means navX
 	
 	every part of an assembeAutonomous() has two parts: 
 	the limiter (put in the condition of the if statement) and the driver (put in the body of the if statement). 
@@ -16,14 +15,15 @@ public class Autonomous2 {
 	
 	Timer mainS = new Timer();
 	static double navXStart = Robot.drivebase.navXYaw;
-	double totalTurnt = 0;
+	double totalDeltaYaw = 0;
 	double oldNavX = 0;
 	double newNavX = 0;
+	double deltaNavX = 0;
 	int[] sSwitch = {0, 3, 6, 9, 15}; //stores seconds to switch autonomous parts
 	int[] eSwitch = {0, 300, 600, 900, 1500}; //stores encoder ticks to switch autonomous parts
 	double[] nSwitch = {navXStart, angleCorrect(navXStart+90), angleCorrect(navXStart+145)}; //stores objective angles for navX turning, used to switch autonomous parts
-	double[] nTotalTurntAtParts = {0, 0, 0, 0, 0, 0}; // ex. first element is total space between element one and two of nSwitch
-	boolean[] operationsCompleted = {false, false, false, false, false, false};
+	double[] nDeltaYaw = {0, 0, 0, 0, 0, 0}; // ex. first element is total space between element one and two of nSwitch
+	boolean[] segmentsCompleted = {false, false, false, false, false, false};
 	
 	public void run() {
 		switch(Robot.autoSelected1) {
@@ -40,7 +40,8 @@ public class Autonomous2 {
 		}
 		oldNavX = newNavX;
 		newNavX = Robot.drivebase.navXYaw;
-		totalTurnt = totalTurnt + (Math.abs(newNavX - oldNavX));
+		deltaNavX = newNavX - oldNavX;
+		totalDeltaYaw = totalDeltaYaw + (Math.abs(deltaNavX));
 	}
 	
 	public Autonomous2() {
@@ -53,9 +54,9 @@ public class Autonomous2 {
 		
 		for(int i=0;i<nSwitch.length;i++) {
 			if(i>0) {
-				nTotalTurntAtParts[i] = nTotalTurntAtParts[i-1] + (nSwitch[i+1] - nSwitch[i]);
+				nDeltaYaw[i] = nDeltaYaw[i-1] + (nSwitch[i+1] - nSwitch[i]);
 			}
-			else {nTotalTurntAtParts[i] = nSwitch[i+1];}
+			else {nDeltaYaw[i] = nSwitch[i+1];}
 		}
 		
 		mainS.start();
@@ -64,49 +65,50 @@ public class Autonomous2 {
 	//tier 1 methods 
 	public void assembleAutonomousOne() { 
 		
-		operationsCompleted[0] = getOperationFinished("s", 0);
-		operationsCompleted[1] = getOperationFinished("e", 1);
-		operationsCompleted[2] = getOperationFinished("n", 2);
-		
-		if(isSeconds(0, 1, 0)) { //part 1
+		segmentsCompleted[0] = getSegmentsFinished("s", 0);
+		segmentsCompleted[1] = getSegmentsFinished("e", 1);
+		segmentsCompleted[2] = getSegmentsFinished("n", 2);
+				
+		if(timeBySeconds(1, 0)) { //part 1
 			driveForwardNoCorrection(0.80, 0.80);
 		}		
-		else if(isEncoders(0, 1, 1)) { //part 2
+		else if(timeByEncoders(1, 1)) { //part 2
 			driveForwardNavXCorrection(0.75, 1, 1);
 		}
-		else if(isNavX(1, 2)) { //part 3
+		else if(timeByNavX(1, 2)) { //part 3
 			turnL(0.60, true);
 		}
 		
 	}
 	
-   //tier 2 methods
+    //tier 2 methods
+	//tier 2 method parameters run by 'name(upper target, int currentOperation)'
 	
-	public boolean isEncoders(int eLow, int eHigh, int currentOperation) { //used for delta encoders
-		return (getAllValuesTrue(currentOperation) && isBetween(Math.abs(Robot.drivebase.lDriveEncDist), eSwitch[eLow], eSwitch[eHigh])); //used for encoder distance)
+	public boolean timeByEncoders(int eHigh, int currentOperation) { //used for delta encoders
+		return (checkSegmentsFinished(currentOperation) && isBetween(Math.abs(Robot.drivebase.lDriveEncDist), 0, eSwitch[eHigh])); //used for encoder distance)
 	}
 	
-	public boolean isSeconds(int sLow, int sHigh, int currentOperation) { //used for delta seconds
-		return (getAllValuesTrue(currentOperation) && isBetween(mainS.get(), sSwitch[sLow], sSwitch[sHigh]));
+	public boolean timeBySeconds(int sHigh, int currentOperation) { //used for delta seconds
+		return (checkSegmentsFinished(currentOperation) && isBetween(mainS.get(), 0, sSwitch[sHigh]));
 	}
 	
-	public boolean isNavX(int nTarget, int currentOperation) { //used for delta navX
-		return (getAllValuesTrue(currentOperation) && isBetween(Robot.drivebase.navXYaw, nSwitch[nTarget]-2, nSwitch[nTarget]+2));
+	public boolean timeByNavX(int nTarget, int currentOperation) { //used for delta navX
+		return (checkSegmentsFinished(currentOperation) && isBetween(Robot.drivebase.navXYaw, nSwitch[nTarget]-2, nSwitch[nTarget]+2));
 	}
 	
 	
 	//tier 3 methods
 	
-	public boolean getOperationFinished(String esn, int number) {
+	public boolean getSegmentsFinished(String esn, int number) {
 		switch(esn) {
 			case "s" : {
 				return (mainS.get()>sSwitch[number]);
 			}
 			case "e" : {
-				return (Robot.drivebase.lDriveEncDist>eSwitch[number]);
+				return (Math.abs(Robot.drivebase.lDriveEncDist)>eSwitch[number]);
 			}
 			case "n" : {
-				return (totalTurnt>nTotalTurntAtParts[number]);
+				return (totalDeltaYaw>nDeltaYaw[number]);
 			}
 			default : {
 				return false;
@@ -114,9 +116,9 @@ public class Autonomous2 {
 		}
 	}
 	
-	public boolean getAllValuesTrue(int currentOperation) {
+	public boolean checkSegmentsFinished(int currentOperation) {
 		for(int i=0;i<currentOperation;i++) {
-			if(!operationsCompleted[i]) {
+			if(!segmentsCompleted[i]) {
 				return false;
 			}
 		}
