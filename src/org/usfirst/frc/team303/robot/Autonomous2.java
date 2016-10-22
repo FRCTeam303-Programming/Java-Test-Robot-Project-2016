51895d426d0cc2f3329df9604f172c0da56fb939
Bradley Boxer <brad.boxer1@gmail.com>
2016-10-21 20:56:33 -0400

    Worked on Autonomous2
    Autonomous2 bug fixes and structural updates, possibly for a merger
    between the tier 2 limiters and drivers.

--- src/org/usfirst/frc/team303/robot/Autonomous2.java
+++ src/org/usfirst/frc/team303/robot/Autonomous2.java
@@ -8,9 +8,6 @@ public class Autonomous2 {
 	e means encoders
 	s means seconds
 	n means navX
-	
-	every part of an assembeAutonomous() has two parts: 
-	the limiter (put in the condition of the if statement) and the driver (put in the body of the if statement). 
 	*/
 	
 	Timer mainS = new Timer();
@@ -24,6 +21,10 @@ public class Autonomous2 {
 	double[] nSwitch = {navXStart, angleCorrect(navXStart+90), angleCorrect(navXStart+145)}; //stores objective angles for navX turning, used to switch autonomous parts
 	double[] nDeltaYaw = {0, 0, 0, 0, 0, 0}; // ex. first element is total space between element one and two of nSwitch
 	boolean[] segmentsCompleted = {false, false, false, false, false, false};
+	int sOp = 0;
+	int eOp = 0;
+	int nOp = 0;
+	int totalOp = 0;
 	
 	public void run() {
 		
@@ -72,12 +73,15 @@ public class Autonomous2 {
 	
 	
 	//tier 1 methods 
+	//every part of an assembeAutonomous() has two parts: 
+	//the limiter (put in the condition of the if statement) and the driver (put in the body of the if statement). 
 	public void assembleAutonomousOne() { 
 		
-		segmentsCompleted[0] = getSegmentsFinished(timeType.SECONDS, 0);
-		segmentsCompleted[1] = getSegmentsFinished(timeType.ENCODERS, 1);
-		segmentsCompleted[2] = getSegmentsFinished(timeType.NAVX, 2);
-				
+		segmentsCompleted[0] = figureSegmentFinished(timeType.SECONDS, 0);
+		segmentsCompleted[1] = figureSegmentFinished(timeType.ENCODERS, 1);
+		segmentsCompleted[2] = figureSegmentFinished(timeType.NAVX, 2);
+		totalOp = sOp + eOp + nOp;
+		
 		if(timeBySeconds(1, 0)) { //part 1
 			driveForwardNoCorrection(0.80, 0.80);
 		}		
@@ -93,31 +97,38 @@ public class Autonomous2 {
     //tier 2 methods
 	//tier 2 method parameters run by 'name(upperTarget, currentOperation)'
 	
+	/* tier 2 methods have two parts: the limiter (used in the isBetween(), basically a less than statement)
+	and the driver (used in checkSegmentsFinished(), basically a greater than statement)
+	possibly combine limiter and driver for tier 2 methods */
+	
 	public boolean timeByEncoders(int eHigh, int currentOperation) { //used for delta encoders
-		return (checkSegmentsFinished(currentOperation) && isBetween(Math.abs(Robot.drivebase.lDriveEncDist), 0, eSwitch[eHigh])); //used for encoder distance)
+		return (!getSegmentFinished(currentOperation) && (Math.abs(Robot.drivebase.lDriveEncDist) < eSwitch[eHigh])); //used for encoder distance)
 	}
 	
 	public boolean timeBySeconds(int sHigh, int currentOperation) { //used for delta seconds
-		return (checkSegmentsFinished(currentOperation) && isBetween(mainS.get(), 0, sSwitch[sHigh]));
+		return (!getSegmentFinished(currentOperation) && (mainS.get() < sSwitch[sHigh]));
 	}
 	
 	public boolean timeByNavX(int nTarget, int currentOperation) { //used for delta navX
-		return (checkSegmentsFinished(currentOperation) && isBetween(Robot.drivebase.navXYaw, nSwitch[nTarget], nSwitch[nTarget]+2));
+		return (!getSegmentFinished(currentOperation) && isBetween(Robot.drivebase.navXYaw, nSwitch[nTarget], nSwitch[nTarget]+2));
 	}
 	
 	
 	//tier 3 methods
 	
-	public boolean getSegmentsFinished(timeType var, int number) {
+	public boolean figureSegmentFinished(timeType var, int OpNum) { //adding 1 for conversion between op number and switches which start with 0
 		switch(var) {
 			case SECONDS : {
-				return (mainS.get()>sSwitch[number]);
+				sOp++;
+				return (mainS.get()>sSwitch[(OpNum+1)-eOp-nOp]);
 			}
 			case ENCODERS : {
-				return (Math.abs(Robot.drivebase.lDriveEncDist)>eSwitch[number]);
+				eOp++;
+				return (Math.abs(Robot.drivebase.lDriveEncDist)>eSwitch[(OpNum+1)-sOp-nOp]);
 			}
 			case NAVX : {
-				return (totalDeltaYaw>nDeltaYaw[number]);
+				nOp++;
+				return (totalDeltaYaw>nDeltaYaw[(OpNum+1)-sOp-eOp]);
 			}
 			default : {
 				return false;
@@ -125,7 +136,7 @@ public class Autonomous2 {
 		}
 	}
 	
-	public boolean checkSegmentsFinished(int currentOperation) {
+	public boolean getSegmentFinished(int currentOperation) {
 		return segmentsCompleted[currentOperation];
 		/* for(int i=0;i<currentOperation;i++) {
 			if(!segmentsCompleted[i]) {
